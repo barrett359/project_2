f27d143bcdec3035cab858acbe021dbe4fb1632e
Bryan Molin <bjmolin@g.clemson.edu>
2024-03-22 18:28:19 -0400

    push

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 //
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-       printf("Not enough arguments, exiting\n");
+       printf("Not enough arguments, exiting\nUse <IP> <port>\n");
        return 0;
     }
     char *serverIP = argv[1];
